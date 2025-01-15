16cc3e04df100aae39ebcc4f81e066289150f0ad
Bunny <144216222+B3nny444@users.noreply.github.com>
2025-01-15 19:18:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ bunny = Client(
 )
 @bunny.on_message(filters.command("start"))
 async def start_cmd(Client, message):
-  print("START Command")
+  await message.reply_text("hi itz bunny time")
+@bunny.on_message(filters.command("help"))
+async def help_cmd(Client, message):
+  await message.reply_text("feel free to ask call me")
+                  
 
 
 print("bot was started")
