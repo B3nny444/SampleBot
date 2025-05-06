e1cd063330f0b497ecb3f5517b3a57b3c75589ee
Bunny <144216222+B3nny444@users.noreply.github.com>
2025-05-06 02:02:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-from pyrogram import Client, filters
+from pyrogram import  Client,filters
 
-API_ID = "27094161"
+BOT_TOKEN ="8029964850:AAH4tP4lunEiU6E-hBbFMx_p9omUMWm5K-Y"
+API_ID ="27094161"
 API_HASH = "39477b23f5e6abea95fe0f92b7f00de0"
-BOT_TOKEN = "7425029151:AAHkMU98HkWgamjUFcGBKZWdEx3_O9lN7Jo"
 
-bunny = Client(
-  name="bunny",
-  api_id = API_ID,
-  api_hash = API_HASH,
-  bot_token = BOT_TOKEN
+Bunny = Client(
+    name = "bunnyBot",
+    api_id = API_ID,
+    api_hash = API_HASH,
+    bot_token = BOT_TOKEN
 )
+@Bunny.on_message(filters.command("start"))
+async def start_cmd(client, message):
+    await message.reply_txt("hi")
 
-START_BUTTONS =[[
-  inlineKeyboardButton("join my channel", url = "https://t.me/Kaiju_no8_in_tamil_dub")
-]]
-@bunny.on_message(filters.command("start"))
-async def start_cmd(Client, message):
-  await message.reply_photo(
-    photo = "",
-    text = "you have to join the channel to start me"
-    reply_markup = inlineKeyboardMarkUp("START_BUTTONS")
-    description = "hi im summoned by Bunny")
-  )
-  
+print("bot started")
 
-                  
-
-
-print("bot was started")
-bunny.run()
+Bunny.run()
